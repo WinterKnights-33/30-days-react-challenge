06d97d49da00eeaf57c6c3f7a06a12d44d600aae
Elie Knight <elie.r.knight@gmail.com>
2022-11-07 21:09:59 -0700

    Update array.js

--- array.js
+++ array.js
@@ -20,3 +20,45 @@ const upper = itCompanies.map(element => {
     return element.toUpperCase();
 });
 console.log(upper);
+
+let arfr = [3, 4, 1, 2, 5];
+console.log(arr.length);
+console.log(arr[0]);
+console.log(arr[2]);
+console.log(arr[4]);
+
+let mixeddDataTypes = ['string', 'number', 3, 3.14, -4, -5];
+
+let itCodmpanies = ['Facebook', 'Goggle', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'];
+console.log(itCompanies);
+console.log(itCompanies.length);
+console.log(itCompanies[0]);
+console.log(itCompanies[3]);
+console.log(itCompanies[7]);
+console.log(itCompanies.join('\n'));
+
+const updper = itCompanies.map(element => {
+    return element.toUpperCase();
+});
+console.log(upper);
+
+let adrr = [3, 4, 1, 2, 5];
+console.log(arr.length);
+console.log(arr[0]);
+console.log(arr[2]);
+console.log(arr[4]);
+
+let mixedDatafTypes = ['string', 'number', 3, 3.14, -4, -5];
+
+let itCompfanies = ['Facebook', 'Goggle', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'];
+console.log(itCompanies);
+console.log(itCompanies.length);
+console.log(itCompanies[0]);
+console.log(itCompanies[3]);
+console.log(itCompanies[7]);
+console.log(itCompanies.join('\n'));
+
+const upfper = itCompanies.map(element => {
+    return element.toUpperCase();
+});
+console.log(upper);
